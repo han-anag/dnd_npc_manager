24e61f28477d0b132875ff1073cc2d1d1ab949f5
han-anag <114708039+han-anag@users.noreply.github.com>
2023-09-06 00:06:50 -0400

    Added different NPC classes

--- dnd_npcs.py
+++ dnd_npcs.py
@@ -1,14 +1,51 @@
+"""
+Simple NPCs have names, languages, race, and statuses
+
+Detailed NPCs have names, base stats, race, languages, ac, speed, and statuses
+
+Story NPCs have names, base stats, race, languages, ac, speed, HP, skills, attacks, spells, and statuses
+"""
 class npc:
-    def __init__(self, firstname = '', lastname = '', race = ''):
+    def __init__(self, firstname = '', lastname = '', race = '', status = ''):
         self.first_name = firstname
         self.last_name = lastname
         self.race = race
+        self.status = status
     
-    def return_first_name(self):
+    def get_first_name(self):
         return self.first_name
     
-    def return_last_name(self):
+    def get_last_name(self):
         return self.last_name
     
-    def return_race(self):
+    def get_race(self):
         return self.race
+
+
+class detailed_npc(npc):
+    def __init__(self, firstname = '', lastname = '', race = '', languages = 'Common', ac = 10, speed = 30, status = ''):
+        super().__init__(firstname, lastname, race, status)
+        self.languages = languages
+        self.ac = ac
+        self.speed = speed
+
+
+"""
+Update to have skills, attacks, and spells later
+"""
+class story_npc(detailed_npc):
+    def __init__(self, firstname = '', lastname = '', race = '', languages = 'Common', ac = 10, speed = 30, hp = 20, status = ''):
+        super().__init__(firstname, lastname, race, languages, ac, speed, status)
+        self.hp =hp
+
+
+def main():
+    bob = npc("Bob", "Smith", "Human")
+    print(bob.get_race())
+
+    alice = story_npc("Alice", "Smith", "Human", "Common", 13, 30, 25)
+    print(alice.ac)
+
+
+if __name__ == "__main__":
+    main()
